aaaf3dd7140b2229753a6749f5abd7b831008081
Manu <manu@snapdragon.cc>
2026-05-05 13:52:39 +0100

    Split megamodules into focused submodules

---
+++ crates/vykar-gui/src/file_tree/slint_model.rs
@@ -0,0 +1,27 @@
+use slint::SharedString;
+
+use crate::TreeRowData;
+
+use super::FileTree;
+
+impl FileTree {
+    pub fn to_slint_model(&self) -> Vec<TreeRowData> {
+        self.visible_rows
+            .iter()
+            .map(|&idx| {
+                let node = &self.arena[idx];
+                TreeRowData {
+                    name: SharedString::from(&node.name),
+                    type_str: SharedString::from(&node.entry_type),
+                    permissions: SharedString::from(&node.permissions),
+                    size_str: SharedString::from(&node.size_str),
+                    depth: node.depth as i32,
+                    is_dir: node.is_dir,
+                    expanded: node.expanded,
+                    check_state: node.check_state as i32,
+                    node_index: idx as i32,
+                }
+            })
+            .collect()
+    }
+}
